232c2adf185bd6489b8d400cc96245820cc25ca3
MmouseLee <mia.lee2076@gmail.com>
2022-06-17 08:42:30 -0700

    Updated Abut.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import myPicture from "../assets/myPicture.jpg";
+
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+        <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src = {myPicture}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Mia Lee</div>
+        <div className="brief_description">
         </div>
       </div>
-    );
+    </div>
+  </div>
+
+      </div>
+    )
   }
 }
